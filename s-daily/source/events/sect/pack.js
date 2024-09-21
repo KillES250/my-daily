ea1e7b6111e55138b27fad6d4f6951587512b121
KillES250 <3479000843@qq.com>
2024-09-21 11:53:25 +0800

    config

--- s-daily/source/events/sect/pack.js
+++ s-daily/source/events/sect/pack.js
@@ -3,7 +3,7 @@ const logger = require('../../librarys/logger');
 module.exports = function (data) {
   const items = data.items;
   items.forEach(item => {
-    if (item.name.includes('<hig>背包扩充石</hig>')) {
+    if (item.name.includes(`<hig>背包扩充石</hig>`)) {
       const id = item.id;
       this.cmd.send(`use ${item.id}`);
       logger.warning(`「${this.userConfig.name}」背包已扩充`);
--- s-daily/source/events/sect/tip.js
+++ s-daily/source/events/sect/tip.js
@@ -25,14 +25,14 @@ module.exports = function (tip) {
 
   if (tip.includes('你拿不下那么多东西')) {
     logger.warning(`「${this.userConfig.name}」背包已满`);
-    this.cmd.send('shop 2 1');
-    this.cmd.send('pack');
+    this.cmd.send(`shop 2 1`);
+    this.cmd.send(`pack`);
     this.socketClose();
   }
 
   if (tip.includes('你没有那么多的钱')) {
     logger.warning(`「${this.userConfig.name}」黄金不足`);
-    this.cmd.send('wakuang');
+    this.cmd.send(`wakuang`);
     this.socketClose();
   }
 
