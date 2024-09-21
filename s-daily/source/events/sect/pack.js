e0006b9d0b754a0aa771af4665b19d44a09d5cf4
KillES250 <3479000843@qq.com>
2024-09-21 11:49:01 +0800

    config

--- s-daily/source/events/sect/pack.js
+++ s-daily/source/events/sect/pack.js
@@ -1,9 +1,12 @@
+const logger = require('../../librarys/logger');
+
 module.exports = function (data) {
   const items = data.items;
   items.forEach(item => {
     if (item.name.includes('<hig>背包扩充石</hig>')) {
       const id = item.id;
-      this.cmd.send(`use ${item.id}`); // 使用反引号和模板字符串
+      this.cmd.send(`use ${item.id}`);
+      logger.warning(`「${this.userConfig.name}」背包已扩充`);
     }
   });
 };
