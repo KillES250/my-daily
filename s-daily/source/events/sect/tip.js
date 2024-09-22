90eb9406f6ee844bb32630b1f4f0962bcce6b89d
KillES250 <3479000843@qq.com>
2024-09-22 09:44:41 +0800

    config

--- s-daily/source/events/sect/tip.js
+++ s-daily/source/events/sect/tip.js
@@ -24,8 +24,7 @@ module.exports = function (tip) {
   }
 
   if (tip.includes('你拿不下那么多东西')) {
-    this.cmd.send('shop 2 1');
-    this.cmd.send('pack');
+    this.cmd.send('shop 2 1;pack');
     logger.warning(`「${this.userConfig.name}」背包已满`);
     this.socketClose();
   }
