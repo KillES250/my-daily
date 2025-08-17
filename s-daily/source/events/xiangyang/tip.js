c5973ab764d3727bb0d2adbf124452e7ebb51e0b
KillES250 <73060560+KillES250@users.noreply.github.com>
2025-08-18 00:46:15 +0800

    Update tip.js

--- s-daily/source/events/xiangyang/tip.js
+++ s-daily/source/events/xiangyang/tip.js
@@ -13,7 +13,7 @@ module.exports = function (tip) {
   }
 
   if (tip.includes('再次进入襄阳城') || tip.includes('你还是不要去添乱了')) {
-    this.cmd.send(`tm 2`);
+    this.cmd.send('tm sb');
   }
 
   if (tip.includes('你挥着铁镐开始认真挖矿')) {
